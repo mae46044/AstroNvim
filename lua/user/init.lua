c319118aa11685866a10cbe8c9f901f41618abd1
mae46044 <mae46044@gmail.com>
2023-05-25 13:15:57 +0900

    js, luaのformat on saveをデフォルトOFFに変更

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -139,8 +139,9 @@ local config = {
     formatting = {
       format_on_save = true, -- enable or disable auto formatting on save
       disabled = { -- disable formatting capabilities for the listed clients
-        -- "sumneko_lua",
-                                "intelephense",
+          "sumneko_lua",
+          "intelephense",
+          "tsserver",
       },
       -- filter = function(client) -- fully override the default formatting function
       --   return true
